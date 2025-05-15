1ad91fa0ba53e17c32ccb19ef9c74e0b080d6eb8
GrishanyaaShustov <dinsuil275283@gmail.com>
2025-05-15 14:39:26 +0500

    Update SecondPartTests.cs

--- Tests/SecondPartTests.cs
+++ Tests/SecondPartTests.cs
@@ -1,156 +1,125 @@
-namespace Tests;
+using System;
 using Car;
 using NUnit.Framework;
 
-[TestFixture]
-public class MyHashTableTests
+namespace Tests
 {
-    private MyHashTable<Car> hashTable;
-
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class MyHashTableTests
     {
-        hashTable = new MyHashTable<Car>();
-    }
+        private MyHashTable<Car.Car> table;
 
-    [Test]
-    public void Add_ShouldIncreaseCount_WhenElementAdded()
-    {
-        var car = new LightCar();
-        car.RandomInit();
-        hashTable.Add(car);
-        Assert.AreEqual(1, hashTable.Count);
-    }
+        [SetUp]
+        public void Setup()
+        {
+            table = new MyHashTable<Car.Car>();
+        }
 
-    [Test]
-    public void Add_ShouldHandleCollisions_WhenKeyHashesAreEqual()
-    {
-        // Пример для тестирования коллизий
-        var car1 = new LightCar { Brand = "Toyota" };
-        car1.RandomInit();
-        var car2 = new LightCar { Brand = "Honda" };
-        car2.RandomInit();
+        private static LightCar CreateCar(int id)
+        {
+            var car = new LightCar();
+            car.RandomInit();
+            car.CarId = new IdNumber(id);
+            return car;
+        }
 
-        hashTable.Add(car1);
-        hashTable.Add(car2);
+        private class CollidingId : IdNumber
+        {
+            public CollidingId(int number) : base(number) { }
+            public override int GetHashCode() => 42;
+        }
 
-        // Проверяем, что оба объекта добавлены, несмотря на возможную коллизию
-        Assert.AreEqual(2, hashTable.Count);
-    }
+        private static LightCar CreateCollidingCar(int id)
+        {
+            var car = new LightCar();
+            car.RandomInit();
+            car.CarId = new CollidingId(id);
+            return car;
+        }
 
-    [Test]
-    public void Remove_ShouldDecreaseCount_WhenElementRemoved()
-    {
-        var car = new LightCar();
-        car.RandomInit();  // Инициализация случайными данными
-        car.CarId = new IdNumber(1);  // Устанавливаем явно CarId
-        hashTable.Add(car);
-    
-        var carId = car.CarId.Number;
-        Assert.IsTrue(hashTable.Remove(carId));  // Проверяем, что элемент удален
-        Assert.AreEqual(0, hashTable.Count);  // Проверяем, что количество элементов уменьшилось
-    }
+        [Test]
+        public void Add_IncreasesCount()
+        {
+            table.Add(CreateCar(1));
+            Assert.That(table.Count, Is.EqualTo(1));
+        }
 
-    [Test]
-    public void Remove_ShouldReturnFalse_WhenElementNotFound()
-    {
-        var car = new LightCar();
-        car.RandomInit();
-        var carId = car.CarId.Number;
+        [Test]
+        public void Add_HandlesCollisions()
+        {
+            table.Add(CreateCollidingCar(1));
+            table.Add(CreateCollidingCar(2));
+            Assert.That(table.Count, Is.EqualTo(2));
+        }
 
-        hashTable.Add(car);
+        [Test]
+        public void Remove_ExistingElement_DecreasesCount()
+        {
+            var car = CreateCar(10);
+            table.Add(car);
+            var result = table.Remove(10);
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.True);
+                Assert.That(table.Count, Is.EqualTo(0));
+            });
+        }
 
-        // Удаляем несуществующий элемент
-        Assert.IsFalse(hashTable.Remove(carId + 1)); 
-    }
+        [Test]
+        public void Remove_NonExistingElement_ReturnsFalse()
+        {
+            table.Add(CreateCar(11));
+            var result = table.Remove(999);
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.False);
+                Assert.That(table.Count, Is.EqualTo(1));
+            });
+        }
 
-    [Test]
-    public void Find_ShouldReturnElement_WhenFound()
-    {
-        var car = new LightCar();
-        car.RandomInit();
-        hashTable.Add(car);
-        var carId = car.CarId.Number;
+        [Test]
+        public void Find_ReturnsElement_WhenExists()
+        {
+            var car = CreateCar(20);
+            table.Add(car);
+            var found = table.Find(20);
+            Assert.That(found, Is.SameAs(car));
+        }
 
-        var foundCar = hashTable.Find(carId);
-        Assert.IsNotNull(foundCar);
-        Assert.AreEqual(carId, foundCar.CarId.Number);
-    }
+        [Test]
+        public void Find_ReturnsNull_WhenNotExists()
+        {
+            table.Add(CreateCar(21));
+            var found = table.Find(22);
+            Assert.That(found, Is.Null);
+        }
 
-    [Test]
-    public void Find_ShouldReturnNull_WhenElementNotFound()
-    {
-        var car = new LightCar();
-        car.RandomInit();
-        var carId = car.CarId.Number;
+        [Test]
+        public void Contains_ReturnsTrue_IfElementExists()
+        {
+            var car = CreateCar(30);
+            table.Add(car);
+            Assert.That(table.Contains(car), Is.True);
+        }
 
-        hashTable.Add(car);
-        var notFoundCar = hashTable.Find(carId + 1); // Ищем другой ID
+        [Test]
+        public void Contains_ReturnsFalse_IfElementNotExists()
+        {
+            var car = CreateCar(31);
+            Assert.That(table.Contains(car), Is.False);
+        }
 
-        Assert.IsNull(notFoundCar);
-    }
-
-    [Test]
-    public void Clear_ShouldEmptyTable()
-    {
-        hashTable.Add(new LightCar { Brand = "Toyota", Year = 2020, Color = "Black", Price = 10000, Clearance = 150, CarId = new IdNumber(1) });
-        hashTable.Clear();
-
-        Assert.AreEqual(0, hashTable.Count);
-    }
-
-    [Test]
-    public void Contains_ShouldReturnTrue_WhenElementExists()
-    {
-        var car = new LightCar();
-        car.RandomInit();
-        hashTable.Add(car);
-
-        Assert.IsTrue(hashTable.Contains(car));
-    }
-
-    [Test]
-    public void Contains_ShouldReturnFalse_WhenElementDoesNotExist()
-    {
-        var car = new LightCar();
-        car.RandomInit();
-
-        Assert.IsFalse(hashTable.Contains(car));
-    }
-
-    [Test]
-    public void CopyTo_ShouldCopyElementsToArray()
-    {
-        var car1 = new LightCar();
-        car1.RandomInit();
-        var car2 = new BigCar();
-        car2.RandomInit();
-
-        // Добавляем машины в хеш-таблицу
-        hashTable.Add(car1);
-        hashTable.Add(car2);
-
-        // Массив для копирования элементов
-        var array = new Car[2];
-        hashTable.CopyTo(array, 0);
-
-        // Проверяем, что массив был правильно скопирован
-        Assert.AreEqual(2, array.Length);
-        Assert.AreEqual(car1, array[0]);  // Проверяем, что car1 на первой позиции
-        Assert.AreEqual(car2, array[1]);  // Проверяем, что car2 на второй позиции
-    }
-    
-    
-    [Test]
-    public void Remove_ShouldReturnTrue_WhenElementExists()
-    {
-        var car1 = new LightCar { Brand = "Toyota", CarId = new IdNumber(1) };
-        car1.RandomInit();
-        hashTable.Add(car1);
-
-        bool result = hashTable.Remove(1); // Удаление по CarId
-
-        Assert.IsTrue(result); // Элемент должен быть удалён
-        Assert.AreEqual(0, hashTable.Count); // Количество элементов должно быть 0
+        [Test]
+        public void Clear_EmptiesTable()
+        {
+            table.Add(CreateCar(40));
+            table.Add(CreateCar(41));
+            table.Clear();
+            Assert.Multiple(() =>
+            {
+                Assert.That(table.Count, Is.EqualTo(0));
+                Assert.That(table.Find(40), Is.Null);
+            });
+        }
     }
 }
